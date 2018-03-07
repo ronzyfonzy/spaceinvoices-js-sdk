de4153f8fa6b97cec65cd0b9cbcd381c1112b2c4
Robert Tajnšek <robi.tajnsek@gmail.com>
2018-03-07 17:05:57 +0100

    Added dotenv for test env variables

--- test/modules.test.js
+++ test/modules.test.js
@@ -1,35 +1,40 @@
+import dotenv from 'dotenv'
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import config from "../config";
 import SpaceInvoices from "../src/spaceInvoices";
 import { MAccount } from "../src/models";
 import { account } from "../src/modules";
 
+dotenv.config()
 let expect = chai.use(chaiAsPromised).expect;
 
 describe("SpaceInvoices Modules Test", () => {
-  let spaceInvoices = new SpaceInvoices(config.apiToken);
+  let spaceInvoices = new SpaceInvoices(process.env.API_TOKEN);
   let account = null;
 
   describe("account", () => {
     it("authenticate wrong cridentials", () => {
       return expect(spaceInvoices.account.authenticate("text@example.com", "here")).to.eventually.be.rejected;
     });
+
     it("authenticate right cridentials", () => {
-      return expect(spaceInvoices.account.authenticate(config.email, config.password).then(_account => {
+      return expect(spaceInvoices.account.authenticate(process.env.TEST_EMAIL, process.env.TEST_PASSWORD).then(_account => {
         account = _account;
         return Promise.resolve(_account);
       })).to.eventually.be.fulfilled;
     });
+
     it("authenticate response instance", () => {
       return expect(account).to.be.instanceOf(MAccount);
     });
+
     it("get request", () => {
       return expect(spaceInvoices.account.get(account.id).then((data) => {
         return Promise.resolve(data);
       })).to.eventually.be.fulfilled;
     });
   });
+
   describe("organization", () => {
     let organizations = null;
     it("get all organizations", () => {
@@ -38,6 +43,7 @@ describe("SpaceInvoices Modules Test", () => {
         return Promise.resolve(_organizations);
       }).catch(error => { console.log(error) })).to.eventually.be.fulfilled;
     });
+
     it("all organizations instances", () => {
       return expect(organizations).to.be.an('array');
     });
